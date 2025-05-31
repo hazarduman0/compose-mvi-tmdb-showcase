7fa77c647c76706f960b207e89fd736bd0b34e63
Hazar Duman <hazardumann0@gmail.com>
2025-05-31 21:44:29 +0300

    Implement navigation graph with custom animated transitions

---
+++ app/src/main/java/com/hazarduman/cinescope/ui/components/Spacer.kt
@@ -0,0 +1,30 @@
+package com.hazarduman.cinescope.ui.components
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun VerticalSpacer(height: Dp) {
+    Spacer(modifier = Modifier.height(height))
+}
+
+@Composable
+fun HorizontalSpacer(width: Dp) {
+    Spacer(modifier = Modifier.width(width))
+}
+
+@Composable
+fun ColumnScope.Spacer(height: Dp) {
+    Spacer(modifier = Modifier.height(height))
+}
+
+@Composable
+fun RowScope.Spacer(width: Dp) {
+    Spacer(modifier = Modifier.width(width))
+}
